6158fe351ffc6d37498c9d34ee92026aa9ee462d
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-04-21 07:45:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
 if array.length == 1
   return array[0]
-else array.length == 2
+elsif array.length == 2
   array.join(" and ")
+else array.length == 3
+  array [0] << ", " << array [1] << ", and" << array[2]
 end
 end
